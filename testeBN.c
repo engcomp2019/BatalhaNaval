ca60ea6bad1117dd961d129ea98faf6a08700ccd
engcomp2019 <engcomp2019@gmail.com>
2015-03-16 10:25:46 -0300

    Update testeBN.c
    Comentários e exclusão de variáveis sem utilização.

--- testeBN.c
+++ testeBN.c
@@ -21,19 +21,17 @@ void explosao(BITMAP *bmp, int posX, int pos);
 void alpng_init(void);
 volatile long int counter = 0;
 void game_timer(void);
-void mira_timer(void);
+
 BITMAP *buffer;
 
 
 //Função Main
 int main(){
-   
 
   if(!begin()){
   
     closing();
     return 0;
-    
   }
   
   gameLoop();
@@ -41,7 +39,7 @@ int main(){
   closing();
   
   return 0;
-
+  
 }
 END_OF_MAIN();
 
@@ -80,14 +78,14 @@ int begin(void)
 
   if (COLOR_BITS == 32) set_alpha_blender(); // instala o canal alpha (funciona apenas em 32 bits)
 
- /* if (install_sound(DIGI_CARD, MIDI_CARD, NULL) < 0)
+  if (install_sound(DIGI_CARD, MIDI_CARD, NULL) < 0)
   {
     if (install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, NULL) < 0)
     {
       allegro_message("Erro ao inicializar o som");
       return (FALSE);
     }
-  }*/
+  }
 
   return (TRUE);
 }
@@ -116,32 +114,28 @@ int gameLoop(void){
   BITMAP *mouseMira = load_bitmap("ImagensBatalhaNaval/mira.bmp",NULL);
   
   //Explosão
-  int explosionInicialX      = 0;
-  int explosionInicialY      = 256;
+  int explosionInicialX      = 0;                 //Valor inicial de X para a explosão utilizada
+  int explosionInicialY      = 256;               //Valor inicial de Y para a explosão utilizada
   int explosionImgLargura    = 512;               //Largura da imagem
   int explosionImgAltura     = 448;               //Altura da imagem
   int explosionQuadroLargura = 64;                //Largura do quadro a ser desenhado na tela
   int explosionQuadroAltura  = 64;                //Altura do quadro a ser desenhado na tela
   int explosionImgX          = explosionInicialX; //Posição X inicial da imagem, altera a cada quadro desenhado.
   int explosionImgY          = explosionInicialY; //Posição Y inicial da imagem, altera a cada quadro desenhado
-  int explosionX = 0;
-  int explosionY = 0;
-  int flagAtiva  = 0;
+  int explosionX             = 0;                 //Valor de X em que explosão irá aparecer na tela
+  int explosionY             = 0;                 //Valor de Y em que a explosão irá aparecer na tela
+  int flagAtiva              = 0;                 //Indica se a explosão deve acontecer ou não (0 false, 1 true)
   
   //Mira
   int miraQuadroLargura = 47; //Largura do quadro a ser desenhado na tela
   int miraQuadroAltura  = 47; //Altura do quadro a ser desenhado na tela
-
   
-  
-  //Variavies temporarias.
+  //Variavies de controle de tempo de execução.
   long int vel_control  = 0;
-  long int mira_control = 0;
   
   LOCK_VARIABLE(counter);
   
   LOCK_FUNCTION(game_timer);
-  
     
   //While principal
   while (!key[KEY_ESC]){
@@ -153,23 +147,28 @@ int gameLoop(void){
     
     if( counter >= vel_control){
       
+      //Controla clique do botão direito do mouse.
       if(mouse_b & 1 && flagAtiva == 0){
-        
+      
+        /*Variavel mouse_b controla o clique do mouse, para usa-la:
+          if(mouse_b & 1) -> se o botão direito foi acionado
+          if(mouse_b & 2) -> se o botão esquerdo foi acionado)*/
+          
         explosionX = mouse_x - (explosionQuadroLargura / 2);
         explosionY = mouse_y - (explosionQuadroAltura / 2);
-        flagAtiva  = 1;
-        
+        flagAtiva  = 1; 
       }
-        
+      
+      //Realiza a explosão no local aonde o mouse clicou com o botão direito.
       if(explosionImgX <= (explosionImgLargura - explosionQuadroLargura ) && 
-        flagAtiva == 1){
+          flagAtiva == 1){
         
         masked_blit(explosion,buffer,explosionImgX,explosionImgY,explosionX,explosionY,explosionQuadroAltura,explosionQuadroLargura);
         
         explosionImgX = explosionImgX + explosionQuadroLargura;
       }
       else if(explosionImgY <= (explosionImgAltura) &&
-      flagAtiva == 1){
+              flagAtiva == 1){
       
         explosionImgX = 0;
         
@@ -180,16 +179,15 @@ int gameLoop(void){
       else {
         explosionImgX = explosionInicialX;
         explosionImgY = explosionInicialY;
-        flagAtiva = 0;
+        flagAtiva = 0; //Isso faz com que uma explosão não começe sem que outra termine.
       }
     
       /*Desenha mouse na tela com imagem de mira*/
-      masked_blit(mouseMira, buffer, 94, 0, mouse_x -23.5, mouse_y -23.5, miraQuadroAltura, miraQuadroLargura);
+      masked_blit(mouseMira, buffer, 94, 0, mouse_x-(miraQuadroLargura/2), mouse_y-(miraQuadroAltura/2), miraQuadroAltura, miraQuadroLargura);
            
       blit(buffer,screen,0,0,0,0,800,600);
       
       vel_control = counter + 1;
-     
     }
   }
   
