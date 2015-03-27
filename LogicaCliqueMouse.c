eb73508e9d4fdfe2b50942f823199efcd5aefe08
rodrigoords <rodrigoords@hotmail.com>
2015-03-27 10:24:42 -0300

    Logica para eventos do jogo
    Logica para ser utilizada em testes de eventos do jogo

--- LogicaCliqueMouse.c
+++ LogicaCliqueMouse.c
@@ -1,29 +1,38 @@
 int i;
 
-if(mouse_b & 1){
+for (i = 0;i < 100; i ++){
+  
+  if (mouse_x >= gameTabuleiro[i].xInicio && mouse_x <= gameTabuleiro[i].xFinal) && 
+    (mouse_y >= gameTabuleiro[i].yInicio && mouse_y <= gameTabuleiro[i].yFinal ){
+    
+    //TODO Trocar imagem do quadro para "brilhar"
+    
+    if(gameTabuleiro[i].adversario == 's'){
+      
+      //TODO Trocar Mouse
+      
+      if(mouse_b & 1){
+    
+        if(gameTabuleiro[i].temNavio == 'n'){
+          
+          //TODO tiro na agua recebendo posição X e Y do centro 
+          
+        }
+        else if(gameTabuleiro[i].temNavio == 's' && gameTabuleiro[i].fogoAtivo == 'n'){
+          
+          //TODO Tiro no navio recebendo posição X e Y do centro 
+          
+        }
+        else if(gameTabuleiro[i].temNavio == 's' && gameTabuleiro[i].fogoAtivo == 's'){
+        
+          //TODO Bloquear mouse -- Trocar imagem
+        
+        }
+    
+      }
+      
+    } 
 
-	for (i = 0;i < 100; i ++){
-		
-		if (mouse_x >= gameTabuleiro[i].xInicio && mouse_x <= gameTabuleiro[i].xFinal) && 
-			(mouse_y >= gameTabuleiro[i].yInicio && mouse_y <= gameTabuleiro[i].yFinal ){
-			
-			if(gameTabuleiro[i].temNavio == 'n'){
-				
-				//TODO tiro na agua recebendo posição X e Y do centro 
-				
-			}
-			else if(gameTabuleiro[i].temNavio == 's' && gameTabuleiro[i].fogoAtivo == 'n'){
-				
-				//TODO Tiro no navio recebendo posição X e Y do centro 
-				
-			}
-			else if(gameTabuleiro[i].temNavio == 's' && gameTabuleiro[i].fogoAtivo == 's'){
-			
-				//TODO Bloquear mouse -- Trocar imagem
-			
-			}
-			
-		}
-		
-	}
+  }
+  
 }
