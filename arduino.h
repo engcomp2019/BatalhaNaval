c6fc688431cec54d7cde69914357156051ca85ce
engcomp2019 <engcomp2019@gmail.com>
2015-05-06 02:05:05 -0300

    RDS - Teste de Comunicação Arduino

--- arduino.h
+++ arduino.h
@@ -10,13 +10,80 @@
 **************************************************************/
 
 // Bibliotecas (Arquivos de Cabecalho)
+#include <String.h>
+#include <stdio.h>
+#include "PSerial.h"
 
 /*
 ================================================================================
 ================================================================================
 */
 
-ArduinoIteracao(int linha, int coluna){
+void ArduinoInteracao(int Indice){
 
 
-}
+	char str[2];
+	char linha;
+
+	if(indice >= 0 && indice < 10){
+	   	
+	   	linha  = '0';
+	   	coluna = indice;		
+	}
+	else if(indice >= 10 && indice < 20){
+		linha  = '1';
+		coluna = indice - 10;
+	}
+	else if(indice >= 20 && indice < 30){
+		linha  = '2';
+		coluna = indice - 20;
+	}
+	else if(indice >= 30 && indice < 40){
+		linha  = '3';
+		coluna = indice - 30;
+	}  	
+	else if(indice >= 40 && indice < 50){
+		linha  = '4';
+		coluna = indice - 40;
+	}
+	else if(indice >= 50 && indice < 60){
+		linha  = '5';
+		coluna = indice - 50;
+	}
+	else if(indice >= 60 && indice < 70){
+		linha  = '6';
+		coluna = indice - 60;
+	}
+	else if(indice >= 70 && indice < 80){
+		linha  = '7';
+		coluna = indice - 70;
+	}	
+	else if(indice >= 80 && indice < 90){
+		linha  = '8';
+		coluna = indice - 80;
+	}
+	else if(indice >= 90 && indice < 100){
+		linha  = '9';
+		coluna = indice - 90;
+	}
+
+
+	sprintf(str,"%s%d",linha,coluna);
+
+	escreveSerial(0,str,2);
+
+}END_OF_FUNCTION(ArduinoInteracao);
+
+
+void fechaSerial(){
+	
+	finalizaSerial(0);
+
+}END_OF_FUNCTION(fechaSerial);
+
+
+void IniciaSerial(int Porta, int velocidadePorta){
+
+	inicializaSerial(0,Porta,velocidadePorta);
+
+}END_OF_FUNCTION(IniciaSerial);
